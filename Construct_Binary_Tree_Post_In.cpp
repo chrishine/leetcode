1332f178f50b5b556d86dfaaca2917088360c425
chrishine <chrishineleo@gmail.com>
2014-01-28 00:43:09 +0800

    modify two

--- Construct_Binary_Tree_Post_In.cpp
+++ Construct_Binary_Tree_Post_In.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -10,12 +11,38 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder){
+TreeNode *gen(vector<int>& postorder, int start, int end, unordered_map<int, int>& map_index, int start2, int end2){
+	if(start > end || start2 > end2) return NULL;
+	int root_val = postorder[end];
+	int index = map_index[root_val];
+	TreeNode *root = new TreeNode(root_val);
+	root -> right = gen(postorder, end - end2 + index, end - 1, map_index, index + 1, end2);
+	root -> left = gen(postorder, start, end - end2 + index - 1, map_index, start2, index - 1);
+	return root;
+}
 
+TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder){
+	if(inorder.empty() || postorder.empty()) return NULL;
+	unordered_map<int, int> map_index;
+	for(int i = 0; i < inorder.size(); ++i){
+		map_index[inorder[i]] = i;
+	}
+	return gen(postorder, 0, postorder.size() - 1, map_index, 0, inorder.size() - 1);
+}
+
+void preorder(TreeNode *root){
+	if(root){
+		cout << root -> val << " ";
+		preorder(root -> left);
+		preorder(root -> right);
+	}
 }
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	vector<int> postorder = {4,2,3,1};
+	vector<int> inorder = {4,2,1,3};
+	TreeNode *ans = buildTree(inorder, postorder);
+	preorder(ans);
 	return 0;
 }
--- Construct_Binary_Tree_Pre_In.cpp
+++ Construct_Binary_Tree_Pre_In.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -10,24 +11,38 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-TreeNode *gen(vector<int>& preorder, vector<int>& inorder, vector<int>& map, int offset){
-	int root_val = preorder[offset];
-	int index = map[root_val];
+TreeNode *gen(vector<int>& preorder, int start, int end, unordered_map<int, int>& map_index, int start2, int end2){
+	if(start > end || start2 > end2) return NULL;
+	int root_val = preorder[start];
+	int index = map_index[root_val];
 	TreeNode *root = new TreeNode(root_val);
-	root -> left = gen(preorder, inorder[] , map, )
+	root -> left = gen(preorder, start + 1, start + index - start2, map_index, start2, index - 1);
+	root -> right = gen(preorder, start + index - start2 + 1, end, map_index, index + 1, end2);
+	return root;
 }
 
 TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder){
 	if(preorder.empty() || inorder.empty()) return NULL;
-	vector<int> map(inorder.size(), 0);
+	unordered_map<int, int> map_index;
 	for(int i = 0; i < inorder.size(); ++i){
-		map[inorder[i]] = i;
+		map_index[inorder[i]] = i;
 	}
-	return gen(preorder, map, 0);
+	return gen(preorder, 0, preorder.size() - 1, map_index, 0, inorder.size() - 1);
+}
+
+void postorder(TreeNode *root){
+	if(root){
+		postorder(root -> left);
+		postorder(root -> right);
+		cout << root -> val << " ";
+	}
 }
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	vector<int> preorder = {1,3,2,4};
+	vector<int> inorder = {1,2,3,4};
+	TreeNode *ans = buildTree(preorder, inorder);
+	postorder(ans);
 	return 0;
 }
